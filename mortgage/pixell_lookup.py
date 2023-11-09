f53f8a4b74717dc335c8e55f72d5a42b82e8c63d
yshnya <csiscar@academic.rrc.ca>
2023-11-09 10:37:08 -0600

    Set up mortgage rate and frequency classes.

--- mortgage/pixell_lookup.py
+++ mortgage/pixell_lookup.py
@@ -12,5 +12,26 @@ periods are used.
 
 from enum import Enum
 
+
 VALID_AMORTIZATION = [5, 10, 15, 20, 25, 30] 
 
+class MortgageRate(Enum):
+    """
+    This class manages the fixed and 
+    variable mortgage rate
+    """
+    FIXED_5 = 0.0500
+    FIXED_3 = 0.0579
+    FIXED_1 = 0.0589
+    VARIABLE_5 = 0.0650
+    VARIABLE_3 = 0.0660
+    VARIABLE_1 = 0.0679
+
+class MortgageFrequency(Enum):
+    """
+    This class manages the frequency
+    of the mortgage
+    """
+    MONTHLY = 12
+    BI_WEEKLY = 26
+    WEEKLY = 52
--- tests/test_mortgage.py
+++ tests/test_mortgage.py
@@ -1,7 +1,7 @@
 """
 Description: A class used to test the Mortgage class.
-Author: {Student Name}
-Date: {Date}
+Author: Czanel Siscar
+Date: 2023/11/08
 Usage: Use the tests encapsulated within this class to test the MortgagePayment class.
 """
 
