10f5d37f136376bc930ec5b793af2967f939b284
yshnya <csiscar@academic.rrc.ca>
2023-11-09 11:01:41 -0600

    Defined a class called Mortgage and defined an __init__ method

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -5,5 +5,26 @@ Date: 2023/11/08
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
+from mortgage.pixell_lookup import MortgageRate, MortgageFrequency, VALID_AMORTIZATION
 
+class Mortgage:
+    def __init__(self, loan_amount, rate, frequency, amortization):
+        if loan_amount < 0:
+            self.loan_amount = loan_amount
+        else:
+            raise ValueError("Loan Amount must be a positive.")
+        
+        if not isinstance(rate,MortgageRate):
+            raise ValueError("Rate provided is invalid.")
+        else:
+            self.rate = rate
+        
+        if not isinstance(frequency,MortgageFrequency):
+            raise ValueError("Frequency provided is invalid.")
+        else:
+            self.frequency = frequency
 
+        if amortization not in VALID_AMORTIZATION:
+            raise ValueError("Amortization provided is invalid.")
+        else:
+            self.amortization = amortization
